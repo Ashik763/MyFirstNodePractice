8f146fc2eaf828c41784654bf2296a0201a83f38
Ashik Ghosh <63238405+Ashik763@users.noreply.github.com>
2021-03-31 03:07:31 +0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require('express');
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
+const users = ['a','b','c','d','e','f'];
+
 app.get('/',(req,res) => {
-    res.send("thank you very much");
+    const fruit ={
+        product :'ada',
+        price :220
+    }
+    res.send("Node ami onek pari___ thank you very much");
+});
+
+
+app.get('/fruits/banana',(req,res) => {
+    res.send({fruit:'banana',quantity:1000,price:100000});
 })
 
+app.get('/users/:id',(req,res) =>{
+ const id = req.params.id;
+ const name = users[id];
+ res.send({id,name});
 
-app.listen(3000,() => console.log('listening to port 3000'));
+//  console.log(res);
+
+})
+
+app.post('/addUser',(req, res) =>{
+    console.log('post req send');
+}) 
+
+
+
+
+app.listen(4000,() => console.log('listening to port 4000'));
